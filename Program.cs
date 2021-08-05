e344ea88819288d4866026f806c27e8d2ed69326
Austin Pierce <Austinentpierce@gmail.com>
2021-08-05 19:43:57 -0400

    added number of guesses that needed to be made

--- Program.cs
+++ Program.cs
@@ -2,11 +2,48 @@
 
 namespace GuessingGame
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            string secretWord = "wealth";
+            string guess = "";
+            int guessCount = 0;
+            int guessLimit = 3;
+            bool outOfGuesses = false;
+
+
+
+            while (guess != secretWord && !outOfGuesses)
+            {
+                if (guessCount < guessLimit)
+                {
+
+                    Console.Write("Enter guess: ");
+                    guess = Console.ReadLine();
+                    guessCount++;
+
+
+                }
+                else
+                {
+                    outOfGuesses = true;
+                }
+
+            }
+            if (outOfGuesses)
+            {
+                Console.Write("You lose!");
+            }
+            else
+            {
+                Console.Write("You Win!");
+            }
+
+
+
+
+            Console.ReadLine();
+        }
     }
-  }
 }
